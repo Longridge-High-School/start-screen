4ba53378bd073819eba2ea80989f9efffae24846
Arcath <adam@arcath.net>
2023-06-25 21:48:51 +0100

    docs: deploy after prepare directly

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'docsSidebar',
             position: 'left',
             label: 'Docs'
           },
